fa9e22ff563e20f423a23a8ec3577512c1a7176e
Gilbert <gilbertduenas@users.noreply.github.com>
2020-11-11 07:49:16 +0900

    Update Day 0: Weighted Mean.py

--- Day 0: Weighted Mean.py
+++ Day 0: Weighted Mean.py
@@ -1,1 +1,15 @@
 # https://www.hackerrank.com/challenges/s10-weighted-mean/problem
+
+l = []
+
+N = int(input())
+X = [int(x) for x in input().split()]
+W = [int(w) for w in input().split()]
+
+for i in range(0, N):
+    w = X[i] * W[i]
+    l.append(w)
+
+m = round(float(sum(l)/sum(W)), 1)
+
+print(m)
