c074f455e2868ce12824f4f9d0d34079d74535c8
Gilbert <gilbertduenas@users.noreply.github.com>
2020-11-14 10:23:11 +0900

    Update Day 1: Standard Deviation.py

--- Day 1: Standard Deviation.py
+++ Day 1: Standard Deviation.py
@@ -1,2 +1,10 @@
 # https://www.hackerrank.com/challenges/s10-standard-deviation/problem
 
+n = int(input().strip())
+X = [int(x) for x in input().strip().split()]
+
+mean = sum(X) / n
+variance = sum([((x - mean) ** 2) for x in X]) / n
+stddev = variance ** 0.5
+
+print('{0:0.1f}'.format(stddev))
