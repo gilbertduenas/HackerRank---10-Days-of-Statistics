8a7dc4465b7232c5ce1236149d64828e73cff9e3
Gilbert <gilbertduenas@users.noreply.github.com>
2020-11-14 10:21:06 +0900

    Update Day 1: Quartiles.py

--- Day 1: Quartiles.py
+++ Day 1: Quartiles.py
@@ -1,2 +1,20 @@
 # https://www.hackerrank.com/challenges/s10-quartiles/problem
 
+from statistics import median
+
+n=int(input())
+arr=[int(x) for x in input().split()]
+arr.sort()
+t=int(len(arr)/2)
+
+if len(arr)%2==0:
+    L=arr[:t]
+    U=arr[t:]
+
+else:
+    L=arr[:t]
+    U=arr[t+1:]
+    
+print(int(median(L)))
+print(int(median(arr)))
+print(int(median(U)))
