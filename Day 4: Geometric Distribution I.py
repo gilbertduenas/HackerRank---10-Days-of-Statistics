0dc88c83142a38240b890d059e94d74514d9089f
Gilbert <gilbertduenas@users.noreply.github.com>
2020-11-18 07:39:27 +0900

    Update Day 4: Geometric Distribution I.py

--- Day 4: Geometric Distribution I.py
+++ Day 4: Geometric Distribution I.py
@@ -1,3 +1,8 @@
 # https://www.hackerrank.com/challenges/s10-geometric-distribution-1/problem
 
+prob = [float(i) for i in input().split(' ')]
+ins = int(input())
 
+res = pow(1-float(prob[0]/prob[1]),ins-1) * float(prob[0]/prob[1])
+
+print("{:.3f}".format(res))
