306ba7187cee2f6926ca7343ecac9ae914576d8c
Gilbert <gilbertduenas@users.noreply.github.com>
2020-11-18 08:08:05 +0900

    Update Day 4: Geometric Distribution II.py

--- Day 4: Geometric Distribution II.py
+++ Day 4: Geometric Distribution II.py
@@ -1,3 +1,10 @@
 # https://www.hackerrank.com/challenges/s10-geometric-distribution-2/problem
 
+frac = input().split(' ')
+n = int(input())
 
+p = int(frac[0])/int(frac[1])
+q = 1 - p
+prob = 1 - q**n
+
+print(round(prob,3))
