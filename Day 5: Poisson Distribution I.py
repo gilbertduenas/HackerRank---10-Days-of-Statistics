1dfe6c2caa995e97e679a0a80c99e04731596c85
Gilbert <gilbertduenas@users.noreply.github.com>
2020-11-19 07:18:01 +0900

    Update Day 5: Poisson Distribution I.py

--- Day 5: Poisson Distribution I.py
+++ Day 5: Poisson Distribution I.py
@@ -1,2 +1,10 @@
 # https://www.hackerrank.com/challenges/s10-poisson-distribution-1/problem
 
+from math import factorial, exp
+
+m = float(input())
+x = int(input())
+
+p = ((miu ** x) * exp(-m)) / factorial(x)
+
+print("%.3f" %p)
