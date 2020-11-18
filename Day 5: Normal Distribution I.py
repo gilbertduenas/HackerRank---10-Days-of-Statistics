99d6fee2a487013b7ead482ea5596bfa208374bf
Gilbert <gilbertduenas@users.noreply.github.com>
2020-11-19 08:04:21 +0900

    Update Day 5: Normal Distribution I.py

--- Day 5: Normal Distribution I.py
+++ Day 5: Normal Distribution I.py
@@ -1,1 +1,12 @@
 # https://www.hackerrank.com/challenges/s10-normal-distribution-1/problem
+
+import math
+
+mean, std = map(int, input().split())
+cdf = lambda x: 0.5 * (1 + math.erf((x - mean) / (std * (2 ** 0.5))))
+
+print('{:.3f}'.format(cdf(float(input()))))
+
+x, y = map(int, input().split())
+
+print('{:.3f}'.format(cdf(x) - cdf(y)))
