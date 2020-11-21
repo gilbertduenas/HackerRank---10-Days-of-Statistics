c19fe0de924aecf3fc393966032787aa338e0fc1
Gilbert <gilbertduenas@users.noreply.github.com>
2020-11-21 09:03:28 +0900

    Update Day 6: The Central Limit Theorem II.py

--- Day 6: The Central Limit Theorem II.py
+++ Day 6: The Central Limit Theorem II.py
@@ -1,1 +1,15 @@
 # https://www.hackerrank.com/challenges/s10-the-central-limit-theorem-2/problem
+
+from math import erf,sqrt
+
+m = int(input())
+n = int(input())
+mean = float(input())
+std = float(input())
+
+std = std/sqrt(n)
+y = m/n
+
+cdf = lambda x: 0.5 * (1 + erf((x-mean)/(std*sqrt(2))))
+
+print(round(cdf(y),4))
