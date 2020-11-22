0773d661e0ac602c8bd605ea0edd8eea31089457
Gilbert <gilbertduenas@users.noreply.github.com>
2020-11-22 20:24:44 +0900

    Update Day 6: The Central Limit Theorem III.py

--- Day 6: The Central Limit Theorem III.py
+++ Day 6: The Central Limit Theorem III.py
@@ -1,1 +1,11 @@
 # https://www.hackerrank.com/challenges/s10-the-central-limit-theorem-3/problem
+
+mu, sigma = 500, 80
+
+muS, sigmaS = mu, sigma/(100**0.5)
+
+A = mu - (1.96*sigmaS)
+B = mu + (1.96*sigmaS)
+
+print(round(A,2))
+print(round(B,2))
