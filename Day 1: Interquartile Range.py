260b4c13d653159235df6fd9b876bad14ce5128d
Gilbert <gilbertduenas@users.noreply.github.com>
2020-12-17 07:17:35 +0900

    Update Day 1: Interquartile Range.py

--- Day 1: Interquartile Range.py
+++ Day 1: Interquartile Range.py
@@ -3,6 +3,13 @@
 # Interquartile ranges are good for showing how spread out your data is.
 # Use this in conjunction with median and total range to see how the data clusters around the mean.
 
+# The first quartile (Q1) is defined as the middle number between the smallest number (minimum) and the median of the data set. 
+# It is also known as the lower or 25th empirical quartile, as 25% of the data is below this point.
+
+# The second quartile (Q2) is the median of a data set; thus 50% of the data lies below this point.
+# The third quartile (Q3) is the middle value between the median and the highest value (maximum) of the data set. 
+# It is known as the upper or 75th empirical quartile, as 75% of the data lies below this point.
+
 import statistics as stat
 
 n = int(input())
