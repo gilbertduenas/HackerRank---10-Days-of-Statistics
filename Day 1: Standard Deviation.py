6d323f80452002ce87a7e227d3d9e4a9c46ae554
Gilbert <gilbertduenas@users.noreply.github.com>
2020-12-19 21:14:12 +0900

    Update Day 1: Standard Deviation.py

--- Day 1: Standard Deviation.py
+++ Day 1: Standard Deviation.py
@@ -5,6 +5,9 @@
 # A low standard deviation indicates that the values tend to be close to the mean (also called the expected value) of the set, 
 # while a high standard deviation indicates that the values are spread out over a wider range.
 
+# The standard deviation of a population and the standard error of a statistic derived from that population (e.g., mean) are quite different,
+# but related by the inverse of the square root of the number of observations. 
+
 n = int(input().strip())
 X = [int(x) for x in input().strip().split()]
 
