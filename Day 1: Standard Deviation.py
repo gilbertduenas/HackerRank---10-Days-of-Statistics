dc31f76eea73b94c40ec7a7872ed5aec6ec5bdd4
Gilbert <gilbertduenas@users.noreply.github.com>
2020-12-19 19:10:20 +0900

    Update Day 1: Standard Deviation.py

--- Day 1: Standard Deviation.py
+++ Day 1: Standard Deviation.py
@@ -2,6 +2,9 @@
 
 # This is the amount of variation or dispersion of a set of values.
 
+# A low standard deviation indicates that the values tend to be close to the mean (also called the expected value) of the set, 
+# while a high standard deviation indicates that the values are spread out over a wider range.
+
 n = int(input().strip())
 X = [int(x) for x in input().strip().split()]
 
