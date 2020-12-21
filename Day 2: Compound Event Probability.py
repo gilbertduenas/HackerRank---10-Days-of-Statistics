33000060884764227893f33550514fbbf0ddd452
Gilbert <gilbertduenas@users.noreply.github.com>
2020-12-22 07:27:21 +0900

    Update Day 2: Compound Event Probability.py

--- Day 2: Compound Event Probability.py
+++ Day 2: Compound Event Probability.py
@@ -1,3 +1,5 @@
 # https://www.hackerrank.com/challenges/s10-mcq-3/problem
 
 # This is the likeliness of two independent events occurring. 
+
+# Compound probability is equal to the probability of the first event multiplied by the probability of the second event.
