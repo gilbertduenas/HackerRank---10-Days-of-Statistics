c4cf900c4f3cc43e5cfac39b2dc9b55f9e9a0ad2
Gilbert <gilbertduenas@users.noreply.github.com>
2020-12-21 18:31:20 +0900

    Update Day 2: Basic Probability.py

--- Day 2: Basic Probability.py
+++ Day 2: Basic Probability.py
@@ -1,1 +1,9 @@
 # https://www.hackerrank.com/challenges/s10-mcq-1/problem
+
+# Any probability is a number between 0 and 1.
+# The probability of the sample space (total outcomes) is 1.
+
+n = number_of_favorable_outcomes
+t = total_outcomes
+
+P(a) = n/t
