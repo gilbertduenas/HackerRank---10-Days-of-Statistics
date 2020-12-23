5cbb7f888e82b91a6edaf3aefad812fbaa1b650e
Gilbert <gilbertduenas@users.noreply.github.com>
2020-12-24 07:39:07 +0900

    Update Day 2: Compound Event Probability.py

--- Day 2: Compound Event Probability.py
+++ Day 2: Compound Event Probability.py
@@ -4,3 +4,6 @@
 
 # Compound probability is equal to the probability of the first event multiplied by the probability of the second event.
 
+# Independent events occur when the outcome of one event does not affect the outcome of the second event. 
+
+# Rolling a four has no effect on tossing a head. 
