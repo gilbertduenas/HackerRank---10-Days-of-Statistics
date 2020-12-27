c4e2005113e56aaad854cfff2c183fb9aa9366a9
Gilbert <gilbertduenas@users.noreply.github.com>
2020-12-28 07:50:51 +0900

    Update Day 3: Drawing Marbles.py

--- Day 3: Drawing Marbles.py
+++ Day 3: Drawing Marbles.py
@@ -1,2 +1,6 @@
 # https://www.hackerrank.com/challenges/s10-mcq-6/problem
 
+# A bag contains 3 red marbles and 4 blue marbles. 
+# Then, 2 marbles are drawn from the bag, at random, without replacement. 
+# If the first marble drawn is red, what is the probability that the second marble is blue?
+
