af67f70a859b922f18616b3e128030f3c223686b
Gilbert <gilbertduenas@users.noreply.github.com>
2020-12-27 19:32:15 +0900

    Update Day 3: Conditional Probability.py

--- Day 3: Conditional Probability.py
+++ Day 3: Conditional Probability.py
@@ -1,1 +1,6 @@
 # https://www.hackerrank.com/challenges/s10-mcq-4/problem
+
+# Suppose a family has 2 children, one of which is a boy. What is the probability that both children are boys?
+
+# Suppose a family has 3 children, one of which is a boy. What is the probability that all children are boys?
+
