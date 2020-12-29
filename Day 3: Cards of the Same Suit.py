a638886aacec5d7e2a461e319ee85ccdcbc4943a
Gilbert <gilbertduenas@users.noreply.github.com>
2020-12-29 20:44:49 +0900

    Update Day 3: Cards of the Same Suit.py

--- Day 3: Cards of the Same Suit.py
+++ Day 3: Cards of the Same Suit.py
@@ -5,3 +5,4 @@
 # What is the probability of drawing 3 cards of the same suit from a 52 card deck without replacing them?
 
 # What is the probability of drawing 4 cards of the same suit from a 52 card deck without replacing them?
+
