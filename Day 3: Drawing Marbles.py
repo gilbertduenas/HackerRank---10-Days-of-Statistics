885637e651cba687d4456939d00f28b39856c0d0
Gilbert <gilbertduenas@users.noreply.github.com>
2020-12-30 10:40:15 +0900

    Update Day 3: Drawing Marbles.py

--- Day 3: Drawing Marbles.py
+++ Day 3: Drawing Marbles.py
@@ -4,3 +4,6 @@
 # Then, 2 marbles are drawn from the bag, at random, without replacement. 
 # If the first marble drawn is red, what is the probability that the second marble is blue?
 
+# A bag contains 4 red marbles and 5 blue marbles. 
+# Then, 3 marbles are drawn from the bag, at random, without replacement. 
+# If the first marble drawn is red, what is the probability that the second marble is blue?
