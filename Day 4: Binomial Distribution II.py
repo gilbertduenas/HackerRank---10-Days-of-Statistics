2b8e7f2733238764d6e110c3374f0f84c3c87932
Gilbert <gilbertduenas@users.noreply.github.com>
2020-12-31 09:31:18 +0900

    Update Day 4: Binomial Distribution II.py

--- Day 4: Binomial Distribution II.py
+++ Day 4: Binomial Distribution II.py
@@ -10,5 +10,6 @@ def b(x, n, p):
     return comb(n, x) * p**x * (1-p)**(n-x)
 
 p, n = list(map(int, input().split(" ")))
+
 print(round(sum([b(i, n, p/100) for i in range(3)]), 3))
 print(round(sum([b(i, n, p/100) for i in range(2, n+1)]), 3))
