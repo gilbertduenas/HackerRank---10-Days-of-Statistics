0e92b5e2e5e5a95456178a4023f28aceaa590c3d
Gilbert <gilbertduenas@users.noreply.github.com>
2021-01-03 20:36:06 +0900

    Update Day 4: Geometric Distribution II.py

--- Day 4: Geometric Distribution II.py
+++ Day 4: Geometric Distribution II.py
@@ -1,5 +1,13 @@
 # https://www.hackerrank.com/challenges/s10-geometric-distribution-2/problem
 
+# A negative binomial experiment is a statistical experiment that has the following properties:
+
+# The experiment consists of N repeated trials.
+# The trials are independent.
+# The outcome of each trial is either success (s) or failure (f).
+# P(s) is the same for every trial.
+# The experiment continues until X successes are observed.
+
 frac = input().split(' ')
 n = int(input())
 
