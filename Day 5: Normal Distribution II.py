f2bdf17fbdfd7cbad67b82e1ccf08d5dffd564e2
Gilbert <gilbertduenas@users.noreply.github.com>
2021-01-05 19:03:51 +0900

    Update Day 5: Normal Distribution II.py

--- Day 5: Normal Distribution II.py
+++ Day 5: Normal Distribution II.py
@@ -2,9 +2,12 @@
 
 import math
 
+# Take the mean and standard deviation and calculate the normal distribution.
 mean, std = map(int, input().split())
 a = int(input())
 b = int(input())
+
+# cumulative distribution function
 cdf = lambda x: 0.5 * (1 + math.erf((x - mean) / (std * (2 ** 0.5))))
 
 print(round((1-cdf(a))*100,2))
