8eb2e53af7bd88a2d7bb2f40dc00fb33c2b1f6fa
Gilbert <gilbertduenas@users.noreply.github.com>
2021-01-09 09:38:29 +0900

    Update Day 5: Normal Distribution II.py

--- Day 5: Normal Distribution II.py
+++ Day 5: Normal Distribution II.py
@@ -1,5 +1,11 @@
 # https://www.hackerrank.com/challenges/s10-normal-distribution-2/problem
 
+# In probability theory, a normal distribution is a type of continuous probability distribution for a real-valued random variable. 
+
+# The normal distribution is a probability distribution. 
+# It is also called Gaussian distribution because it was first discovered by Carl Friedrich Gauss.
+# The normal distribution is a continuous probability distribution that is very important in many fields of science.
+
 import math
 
 # Take the mean and standard deviation and calculate the normal distribution.
@@ -7,7 +13,7 @@ mean, std = map(int, input().split())
 a = int(input())
 b = int(input())
 
-# cumulative distribution function
+# cumulative distribution function is the probability that X will take a value less than or equal to x.
 cdf = lambda x: 0.5 * (1 + math.erf((x - mean) / (std * (2 ** 0.5))))
 
 print(round((1-cdf(a))*100,2))
