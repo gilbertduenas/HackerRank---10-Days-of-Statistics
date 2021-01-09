0bdb71c7ddad3269513250fae3d4c4e297af0418
Gilbert <gilbertduenas@users.noreply.github.com>
2021-01-09 09:45:18 +0900

    Update Day 5: Poisson Distribution I.py

--- Day 5: Poisson Distribution I.py
+++ Day 5: Poisson Distribution I.py
@@ -1,5 +1,9 @@
 # https://www.hackerrank.com/challenges/s10-poisson-distribution-1/problem
 
+# This is a discrete probability distribution that expresses the probability of a given number of events occurring 
+# in a fixed interval of time or space if these events occur with a known constant mean rate and independently of the time since the last event.
+# The Poisson distribution can also be used for the number of events in other specified intervals such as distance, area or volume.
+
 from math import factorial, exp
 
 m = float(input())
