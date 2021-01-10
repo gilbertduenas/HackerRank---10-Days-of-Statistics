dc056ee01377315c6f94baed567e5c79bfccf65c
Gilbert <gilbertduenas@users.noreply.github.com>
2021-01-10 09:22:20 +0900

    Update Day 5: Poisson Distribution I.py

--- Day 5: Poisson Distribution I.py
+++ Day 5: Poisson Distribution I.py
@@ -4,6 +4,12 @@
 # in a fixed interval of time or space if these events occur with a known constant mean rate and independently of the time since the last event.
 # The Poisson distribution can also be used for the number of events in other specified intervals such as distance, area or volume.
 
+# For instance, a call center receives an average of 180 calls per hour, 24 hours a day. 
+# The calls are independent; receiving one does not change the probability of when the next one will arrive. 
+# The number of calls received during any minute has a Poisson probability distribution: the most likely number is 3, 
+# but 2 and 4 are also likely and there is a small probability of it being as low as zero and a very small probability it could be 10. 
+# Another example is the number of decay events that occur from a radioactive source in a given observation period.
+
 from math import factorial, exp
 
 m = float(input())
@@ -12,4 +18,3 @@ x = int(input())
 p = ((miu ** x) * exp(-m)) / factorial(x)
 
 print("%.3f" %p)
-
