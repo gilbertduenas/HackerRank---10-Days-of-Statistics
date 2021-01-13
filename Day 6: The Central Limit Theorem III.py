c27d98d97a54497c17a82df6258f7da56b7e0c8d
Gilbert <gilbertduenas@users.noreply.github.com>
2021-01-14 07:24:37 +0900

    Update Day 6: The Central Limit Theorem III.py

--- Day 6: The Central Limit Theorem III.py
+++ Day 6: The Central Limit Theorem III.py
@@ -1,5 +1,12 @@
 # https://www.hackerrank.com/challenges/s10-the-central-limit-theorem-3/problem
 
+# The central limit theorem (CLT) states that, for a large enough sample (N), 
+# the distribution of the sample mean will approach normal distribution. 
+# This holds for a sample of independent random variables from any distribution with a finite standard deviation.
+
+# When independent random variables are added, their properly normalized sum tends toward a normal distribution 
+# (informally a bell curve) even if the original variables themselves are not normally distributed.
+
 mu, sigma = 500, 80
 
 muS, sigmaS = mu, sigma/(100**0.5)
