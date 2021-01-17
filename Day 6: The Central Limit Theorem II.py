6d18ee88dc1296d4825232e0a40647ee9f9af1b4
Gilbert <gilbertduenas@users.noreply.github.com>
2021-01-18 08:01:37 +0900

    Update Day 6: The Central Limit Theorem II.py

--- Day 6: The Central Limit Theorem II.py
+++ Day 6: The Central Limit Theorem II.py
@@ -4,6 +4,10 @@
 # the distribution of the sample mean will approach normal distribution. 
 # This holds for a sample of independent random variables from any distribution with a finite standard deviation.
 
+# The central limit theorem states that if you have a population with mean μ and standard deviation σ 
+# and take sufficiently large random samples from the population with replacement, 
+# then the distribution of the sample means will be approximately normally distributed.
+
 # When independent random variables are added, their properly normalized sum tends toward a normal distribution 
 # (informally a bell curve) even if the original variables themselves are not normally distributed.
 
