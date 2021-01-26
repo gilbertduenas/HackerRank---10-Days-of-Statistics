bf545a149c40607ff2eb809e10c0359eabc575d9
Gilbert <gilbertduenas@users.noreply.github.com>
2021-01-27 07:21:29 +0900

    Update Day 7: Pearson Correlation Coefficient I.py

--- Day 7: Pearson Correlation Coefficient I.py
+++ Day 7: Pearson Correlation Coefficient I.py
@@ -1,5 +1,9 @@
 # https://www.hackerrank.com/challenges/s10-pearson-correlation-coefficient/problem
 
+# Pearson’s correlation coefficient is the test statistics that measures the statistical relationship, or association, between two continuous variables.  
+# It is known as the best method of measuring the association between variables of interest because it is based on the method of covariance.  
+# It gives information about the magnitude of the association, or correlation, as well as the direction of the relationship.
+
 N = int(input())
 X = list(map(float,input().strip().split()))
 Y = list(map(float,input().strip().split()))
