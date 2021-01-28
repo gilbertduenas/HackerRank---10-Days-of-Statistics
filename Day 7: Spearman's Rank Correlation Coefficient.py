36184df1d77ffaf7bdec3b520f02101079741255
Gilbert <gilbertduenas@users.noreply.github.com>
2021-01-29 08:29:32 +0900

    Update Day 7: Spearman's Rank Correlation Coefficient.py

--- Day 7: Spearman's Rank Correlation Coefficient.py
+++ Day 7: Spearman's Rank Correlation Coefficient.py
@@ -4,6 +4,10 @@
 # is a nonparametric measure of rank correlation (statistical dependence between the rankings of two variables). 
 # It assesses how well the relationship between two variables can be described using a monotonic function.
 
+# The Spearman correlation between two variables is equal to the Pearson correlation between the rank values of those two variables; 
+# while Pearson's correlation assesses linear relationships, Spearman's correlation assesses monotonic relationships (whether linear or not). 
+# If there are no repeated data values, a perfect Spearman correlation of +1 or −1 occurs when each of the variables is a perfect monotone function of the other.
+
 def get_rank(X, n):
     x_rank = dict((x, i+1) for i, x in enumerate(sorted(set(X))))
     return [x_rank[x] for x in X]
