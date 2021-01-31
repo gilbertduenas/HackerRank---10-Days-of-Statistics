5ab43e3e4f210c921845cdd8006c0100b6c450c5
Gilbert <gilbertduenas@users.noreply.github.com>
2021-01-31 10:12:12 +0900

    Update Day 8: Least Square Regression Line.py

--- Day 8: Least Square Regression Line.py
+++ Day 8: Least Square Regression Line.py
@@ -1,5 +1,8 @@
 # https://www.hackerrank.com/challenges/s10-least-square-regression-line/problem
 
+# The Least Squares Regression Line is the line that makes the vertical distance from the data points to the regression line as small as possible. 
+# It's called a “least squares” because the best line of fit is one that minimizes the variance (the sum of squares of the errors).
+
 n = 5
 xy = [map(int, input().split()) for _ in range(n)]
 
