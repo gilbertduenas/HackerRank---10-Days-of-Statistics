4749c772a8d5dcb87a5501be5437145ca59fcc98
Gilbert <gilbertduenas@users.noreply.github.com>
2021-01-31 11:07:16 +0900

    Update Day 8: Pearson Correlation Coefficient II.py

--- Day 8: Pearson Correlation Coefficient II.py
+++ Day 8: Pearson Correlation Coefficient II.py
@@ -3,3 +3,7 @@
 # Pearson’s correlation coefficient is the test statistics that measures the statistical relationship, or association, between two continuous variables.  
 # It is known as the best method of measuring the association between variables of interest because it is based on the method of covariance.  
 # It gives information about the magnitude of the association, or correlation, as well as the direction of the relationship.
+
+# Pearson's correlation coefficient is the covariance of the two variables divided by the product of their standard deviations. 
+# The form of the definition involves a "product moment", that is, 
+# the mean (the first moment about the origin) of the product of the mean-adjusted random variables; hence the modifier product-moment in the name.
