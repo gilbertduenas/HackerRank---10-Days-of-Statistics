0af3fcdc6b4dc24689cbe6db4f529133cd3e63ce
Gilbert <gilbertduenas@users.noreply.github.com>
2021-01-31 10:12:58 +0900

    Update Day 8: Pearson Correlation Coefficient II.py

--- Day 8: Pearson Correlation Coefficient II.py
+++ Day 8: Pearson Correlation Coefficient II.py
@@ -1,1 +1,5 @@
 # https://www.hackerrank.com/challenges/s10-mcq-7/problem
+
+# Pearson’s correlation coefficient is the test statistics that measures the statistical relationship, or association, between two continuous variables.  
+# It is known as the best method of measuring the association between variables of interest because it is based on the method of covariance.  
+# It gives information about the magnitude of the association, or correlation, as well as the direction of the relationship.
