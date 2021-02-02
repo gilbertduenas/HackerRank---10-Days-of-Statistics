a797d49768124fd43993f7a9d61392c5d0ec043b
Gilbert <gilbertduenas@users.noreply.github.com>
2021-02-03 07:39:42 +0900

    Update Day 9: Multiple Linear Regression.py

--- Day 9: Multiple Linear Regression.py
+++ Day 9: Multiple Linear Regression.py
@@ -2,7 +2,7 @@
 
 # Multiple linear regression (MLR), also known simply as multiple regression, 
 # is a statistical technique that uses several explanatory variables to predict the outcome of a response variable. 
-# Multiple regression is an extension of linear (OLS) regression that uses just one explanatory variable.
+# Multiple regression is an extension of linear (OLS) regression that uses just one explanatory variable. 
 
 import numpy as np
 
